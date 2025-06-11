f4bb4bd7df027e6c97fd552e2f39c85734a43747
Preethikha <43961271+preethikha9@users.noreply.github.com>
2025-06-11 23:36:27 +0530

    Create parking_lot.cpp
    requirements

---
+++ parking_lot.cpp
@@ -0,0 +1,10 @@
+/* 
+Requirements
+1. should be able to park car, bike (abstract class vehicle)
+2. multiple floors
+3. multiple entry and exit 
+4. cash / card while exit 
+5. displayboard on each floor 
+6. payment class for calculating payment 
+7. admin and attendent
+*/
