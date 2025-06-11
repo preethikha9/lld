8d5267e844fd4785d4fb0390d2b3b5dc9f43ec08
Preethikha <43961271+preethikha9@users.noreply.github.com>
2025-06-11 23:42:46 +0530

    Update parking_lot.cpp
    added objects

--- parking_lot.cpp
+++ parking_lot.cpp
@@ -6,5 +6,25 @@ Requirements
 4. cash / card while exit 
 5. displayboard on each floor 
 6. payment class for calculating payment 
-7. admin and attendent
+7. admin and Attendant
+
+
+objects associated with this requirements 
+1. Vehicle (Abstract)
+    Car (inherits Vehicle)
+    Bike (inherits Vehicle)
+2. Parking Spot  (Abstract)
+    Car
+    Bike 
+3. Floor
+4. DisplayBoard
+5. Gate  (Abstract)
+    Entry 
+    Exit
+6. Payment
+7. User
+    Admin
+    Attendant
+8. Parking Lot (Singleton class)
 */
+
